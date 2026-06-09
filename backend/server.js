d185c104e187f1987a21689f98c4cd45c30ac944
sezaicnk <sezaicenik6@gmail.com>
2026-06-09 20:56:14 +0300

    güncelleme 2
    güncelleme

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // Load env vars
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const app = express();
 
